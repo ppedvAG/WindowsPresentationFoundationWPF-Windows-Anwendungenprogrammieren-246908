c5308e15c75d41b4bbd08942fb1376adf0725a31
ppedvLukasK <lukask@ppedv.de>
2025-03-26 09:19:24 +0100

    M004

---
+++ M004/FourValueToMarginConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M004;
+
+public class FourValueToMarginConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		//Einzelne Slider Values von object zu double konvertieren
+		double[] d = new double[values.Length];
+		for (int i = 0; i < values.Length; i++)
+			d[i] = (double) values[i];
+
+		return new Thickness(d[0], d[1], d[2], d[3]);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ M004/ScoreToGradeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M004;
+
+public class ScoreToGradeConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		int x = (int) (double) value;
+		switch (x)
+		{
+			case <= 50: return "Ungenügend";
+			case <= 60: return "Mangelhaft";
+			case <= 70: return "Ausreichend";
+			case <= 80: return "Befriedigend";
+			case <= 90: return "Gut";
+			case <= 100: return "Sehr Gut";
+			default: return "Fehler";
+		}
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
