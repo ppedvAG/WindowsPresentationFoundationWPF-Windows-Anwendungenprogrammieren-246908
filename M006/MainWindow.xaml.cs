d71f52e5293e875ba8639acb8672e01369a7d1ac
ppedvLukasK <lukask@ppedv.de>
2025-03-27 10:50:24 +0100

    M006

---
+++ M006/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace M006;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+	private void TestClicked(object sender, RoutedEventArgs e)
+	{
+		//((TextBlock) Resources["TestOutput"]).Foreground = new SolidColorBrush(Colors.Green);
+		Resources["DefaultFontSize"] = ((double) Resources["DefaultFontSize"]) + 1;
+    }
+}
