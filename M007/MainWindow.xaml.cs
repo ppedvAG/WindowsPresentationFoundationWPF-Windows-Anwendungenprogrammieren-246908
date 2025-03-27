ed936e309ad2923ed73065f7593e21e87f599c17
ppedvLukasK <lukask@ppedv.de>
2025-03-27 12:24:27 +0100

    M007

---
+++ M007/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M007;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<Fahrzeug> Fahrzeuge { get; set; } = new ObservableCollection<Fahrzeug>
+	{
+		new Fahrzeug(251, FahrzeugMarke.BMW),
+		new Fahrzeug(274, FahrzeugMarke.BMW),
+		new Fahrzeug(146, FahrzeugMarke.BMW),
+		new Fahrzeug(208, FahrzeugMarke.Audi),
+		new Fahrzeug(189, FahrzeugMarke.Audi),
+		new Fahrzeug(133, FahrzeugMarke.VW),
+		new Fahrzeug(253, FahrzeugMarke.VW),
+		new Fahrzeug(304, FahrzeugMarke.BMW),
+		new Fahrzeug(151, FahrzeugMarke.VW),
+		new Fahrzeug(250, FahrzeugMarke.VW),
+		new Fahrzeug(217, FahrzeugMarke.Audi),
+		new Fahrzeug(125, FahrzeugMarke.Audi)
+	};
+
+	public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ M007/MarkeToImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M007;
+
+class MarkeToImageConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		FahrzeugMarke marke = (FahrzeugMarke) value;
+		switch (marke)
+		{
+			case FahrzeugMarke.Audi: return "C:/Users/lk3/source/repos/WPF_2025_03_24/M007/Images/audi.png";
+			case FahrzeugMarke.BMW: return "C:/Users/lk3/source/repos/WPF_2025_03_24/M007/Images/bmw.png";
+			case FahrzeugMarke.VW: return "C:/Users/lk3/source/repos/WPF_2025_03_24/M007/Images/vw.png";
+			default: return string.Empty;
+		}
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
